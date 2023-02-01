daade1034bf93aba7e0f991732b73f569ca85234
hychoe0 <hychoe@umich.edu>
2023-02-01 04:49:49 -0500

    Working on Image Test Cases

--- Image_tests.cpp
+++ Image_tests.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <sstream>
 #include <cassert>
+#include <fstream>
 
 using namespace std;
 
@@ -46,33 +47,68 @@ TEST(test_print_basic) {
   delete img; // delete the Image
 }
 
-// TEST(test_print) {
-//   Image *img = new Image; // create an Image in dynamic memory
+// Basic Test for initialization img and returning width and height
+TEST(test_image_basic) {
+  Image *img = new Image; // create an Image in dynamic memory
 
-//   const Pixel red = {255, 0, 0};
-//   const Pixel green = {0, 255, 0};
-//   const Pixel blue = {0, 0, 255};
-//   //const Pixel white = {255, 255, 255};
+  const int width = 3;
+  const int height = 3;
 
-//   const int width = 3;
-//   const int height = 4;
-//   Image_init(img, width, height);
-//   Image_set_pixel(img, 0, 0, red);
-//   Image_set_pixel(img, 0, 1, green);
-//   Image_set_pixel(img, 0, 2, blue);
-//   // Image_set_pixel(img, 0, 3, white);
+  Image_init(img, width, height);
+  
+  ASSERT_EQUAL(Image_width(img), width);
+  ASSERT_EQUAL(Image_height(img), height);
 
-//   Image_print(img, cout);
+  delete img; // delete the Image
+}
 
-//   ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 0), red));
-//   ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 1), green));
-//   ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 2), blue));
-//   // ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 3), white));
+// Test for reading PPM format and getting the pixel
+TEST(test_image_ppm) {
+  Image *img = new Image; // create an Image in dynamic memory
 
-//   delete img; // delete the Image
-// }
+  string fileName = "crabster_70x35.correct.ppm";
 
-TEST(test_Image_fill) {
+  ifstream input(fileName);
+
+  Image_init(img, input);
+  
+  ASSERT_EQUAL(Image_width(img), 70);
+  ASSERT_EQUAL(Image_height(img), 35);
+
+  ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 0), {255, 255, 255}));
+  ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 22, 1), {181, 137, 143}));
+  ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 34, 69), {255, 255, 255}));
+
+  delete img; // delete the Image
+}
+
+TEST(test_image_print) {
+  Image *img = new Image; // create an Image in dynamic memory
+
+  const Pixel red = {255, 0, 0};
+  const Pixel green = {0, 255, 0};
+  const Pixel blue = {0, 0, 255};
+  //const Pixel white = {255, 255, 255};
+
+  const int width = 3;
+  const int height = 4;
+  Image_init(img, width, height);
+  Image_set_pixel(img, 0, 0, red);
+  Image_set_pixel(img, 0, 1, green);
+  Image_set_pixel(img, 0, 2, blue);
+  // Image_set_pixel(img, 0, 3, white);
+
+  Image_print(img, cout);
+
+  ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 0), red));
+  ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 1), green));
+  ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 2), blue));
+  // ASSERT_TRUE(Pixel_equal(Image_get_pixel(img, 0, 3), white));
+
+  delete img; // delete the Image
+}
+
+TEST(test_image_fill) {
   Image *img = new Image; // create an Image in dynamic memory
 
   Pixel red = {255, 0, 0};
@@ -94,6 +130,7 @@ TEST(test_Image_fill) {
 
   delete img; // delete the Image
 }
+
 
 
 // NOTE: The unit test framework tutorial in Lab 2 originally
